d4399afd1478677de8fa5b551d18e6b9d8c11313
simonbg <simon@LAPTOP-CMEJSS23.localdomain>
2020-05-19 05:44:36 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,351 @@
+import shapefile
+import numpy as np
+import math
+import heapq
+
+
+# constants
+LATITUDE_RANGE = (45.49, 45.53)
+LONGITUDE_RANGE = (-73.59, -73.55)
+GRID_SIDE_LENGTH = 0.002
+
+COST_ALONG_BLOCK = 1.3
+COST_DIAGONAL = 1.5
+COST_STRAIGHT = 1
+
+
+def main():
+
+    # preparation
+
+    sf = shapefile.Reader("Shape/crime_dt.shp")
+    shapes = sf.shapes()
+
+    grid = build_graph(shapes)
+
+    # getting a flat descending order list of references to our graph cells
+    flat_list = [node for row in grid for node in row]
+    flat_list.sort(key=lambda cell: cell.crime_count, reverse=True)
+
+    # specific set-up
+
+    # apply threshold rate
+    threshold = 0.75
+    num_blocks = math.floor((1 - threshold) * len(flat_list))
+
+    # update cells above the threshold to indicate they are blocks
+    for index in range(num_blocks):
+        flat_list[index].block = True
+
+    # mention the brackets
+    # todo check input
+
+    starting_longitude = -73.555
+    starting_latitude = 45.495
+    goal_longitude = -73.585
+    goal_latitude = 45.5275
+
+    starting_node = get_graph_node(grid, starting_longitude, starting_latitude)
+    goal_node = get_graph_node(grid, goal_longitude, goal_latitude)
+
+    # get heuristic evaluations for each cell
+    for node in flat_list:
+        node.h = heuristic(node, goal_node)
+
+    # get specific edges
+    get_edges(grid)
+
+    if not starting_node.edges:
+        message = "No moves available from starting point. Enter another location."
+        #todo go back to main menu
+    if not goal_node.edges:
+        message = "No way to reach goal destination. Enter another destination."
+        #todo go back main menu
+
+    #todo
+    # show graph, print stats
+    print(starting_node.h)
+
+    # A* algorithm
+
+    path = A_star_algorithm(starting_node, goal_node)
+    if len(path) == 1:
+        # todo output message
+        # make sure a point is drawn on drawing + message !!!
+        print("same")
+        pass
+    if not path:
+        #output message no path found
+        print("no solution")
+        pass
+    else:
+        #draw path from path[]
+        print(f"hurrah: {len(path)}")
+        print(path)
+        pass
+
+
+# A point as represented in a 2D cartesian plane
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"{{{self.x}, {self.y}}}"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+# represents a sub-grid within the map
+# the boolean blockmeans that the area will be represented as block due
+# to a crime_count above the threshold
+class GraphNode:
+    def __init__(self, point):
+        self.point = point
+        self.h = -1    # heuristic value
+        self.crime_count = 0
+        self.block = False
+        self.edges = []
+
+
+# represents an edge from one GraphNode to another
+class Edge:
+    def __init__(self, graph_node, cost):
+        self.graph_node = graph_node
+        self.cost = cost
+
+
+# represents a node in the A* algorithm implementation
+class TreeNode:
+    def __init__(self, graph_node, parent, total_cost, A_star_score):
+        self.graph_node = graph_node
+        self.parent = parent
+        self.children = []
+        self.total_cost = total_cost
+        self.A_star_score = A_star_score
+
+    def __lt__(self, other):
+        return self.A_star_score < other.A_star_score
+
+
+# Building graph of points corresponding to bottom left corners of each grids
+# Entering cumulative crime stats or each point
+# Note that we use the conventional x, y cartesian representation to represent points
+# whereas the geolocations in the shapefile use latitude/longitude (y,x)
+def build_graph(shapes):
+    y_axis_size = round((LATITUDE_RANGE[1] - LATITUDE_RANGE[0]) / GRID_SIDE_LENGTH)
+    x_axis_size = round((LONGITUDE_RANGE[1] - LONGITUDE_RANGE[0]) / GRID_SIDE_LENGTH)
+
+    grid = []
+
+    # creating the basic graph nodes
+    for row in range(y_axis_size):
+        grid.append([])
+        for col in range(x_axis_size):
+            point = Point(LONGITUDE_RANGE[0] + GRID_SIDE_LENGTH * col, LATITUDE_RANGE[1] - GRID_SIDE_LENGTH * (row + 1))
+            grid[row].append(GraphNode(point))
+
+    # adding crime stats to appropriate grids
+    for shape in shapes:
+        longitude = shape.points[0][0]
+        latitude = shape.points[0][1]
+        cell = get_graph_node(grid, longitude, latitude)
+        cell.crime_count += 1
+
+    return grid
+
+
+# get a reference to graph node representing a certain geolocation
+def get_graph_node(grid, longitude, latitude):
+    x = math.floor((longitude - LONGITUDE_RANGE[0]) / GRID_SIDE_LENGTH)
+    y = math.floor((LATITUDE_RANGE[1] - latitude) / GRID_SIDE_LENGTH)
+    return grid[y][x]
+
+
+# returns distance between a node and the goal
+# this heuristic is admissible and monotonous.
+# It will never overestimate the true distance
+# and, for cell x and neighbour x2, h(x) <= cost(x, x2) + h(x2)
+# For these reasons, the goal will always be reached by the fastest route
+# and **each node will always be first visited at the lowest cost**
+def heuristic(node, goal_node):
+    y = goal_node.point.y - node.point.y
+    x = goal_node.point.x - node.point.x
+    return math.sqrt(y**2 + x**2)
+
+
+# Since the heuristic function is admissible, when we encounter the goal node, the solution path will
+# also be the most effective path
+# Note: since the heuristic function is monotonous, we store visited nodes in a set, since they will
+# always be visited at the best cost the first time.
+def A_star_algorithm(starting_node, goal_node, ):
+
+    solution_path = []    # a list of points from the goal back to the root of the tree
+    open_list = []        # priority queue, key is A* score, value is tree node reference
+    closed_list = {None}  # this is a set because the heuristic function is monotonous
+
+    root = TreeNode(starting_node, None, 0, starting_node.h)
+    heapq.heappush(open_list, root)
+
+    while open_list:
+
+        #todo chck elapsed time
+
+        node = heapq.heappop(open_list)
+
+        if node.graph_node is goal_node:
+            # we have reached the goal
+            solution_path = get_solution_path(node)
+            return solution_path
+
+        closed_list.add(node.graph_node.point)
+
+        for edge in node.graph_node.edges:
+            if edge.graph_node.point in closed_list:
+                continue  # ignore a visited node
+            total_cost = node.total_cost + edge.cost
+            A_star_score = total_cost + edge.graph_node.h
+            tree_node = TreeNode(edge.graph_node, node, total_cost, A_star_score)
+            # adding new node to the open list, with A* score as key
+            heapq.heappush(open_list, tree_node)
+
+    # if we ended up here, it means we have exhausted all paths
+    # but the search yielded no solution
+    return solution_path
+
+
+def get_solution_path(node):
+    solution_path = [node.graph_node.point]
+    while node.parent:
+        node = node.parent
+        solution_path.append(node.graph_node.point)
+
+    return solution_path
+
+
+# obtain edges for each cell in the grid
+# Edges along the boundaries of the graph aren't allowed
+def get_edges(grid):
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            node = grid[i][j]
+
+            # East-bound edges
+            if j < len(grid[0]) - 1:
+                # if top-row
+                if i == 0:
+                    if node.block and grid[i+1][j].block:
+                        pass  # no east edges
+                    elif node.block:
+                        node.edges.extend([Edge(grid[i][j+1], COST_ALONG_BLOCK),
+                                          Edge(grid[i+1][j+1], COST_DIAGONAL)])
+                    elif grid[i+1][j].block:
+                        node.edges.append(Edge(grid[i][j+1], COST_ALONG_BLOCK))
+                    else:
+                        node.edges.extend([Edge(grid[i][j + 1], COST_STRAIGHT),
+                                          Edge(grid[i + 1][j + 1], COST_DIAGONAL)])
+                # if bottom-row
+                elif i == len(grid) - 1:
+                    if not node.block:
+                        node.edges.append(Edge(grid[i-1][j+1], COST_DIAGONAL))
+                # regular nodes
+                else:
+                    if node.block and grid[i+1][j].block:
+                        pass  # no east edges
+                    elif node.block:
+                        node.edges.extend([Edge(grid[i][j+1], COST_ALONG_BLOCK),
+                                          Edge(grid[i+1][j+1], COST_DIAGONAL)])
+                    elif grid[i+1][j].block:
+                        node.edges.extend([Edge(grid[i-1][j+1], COST_DIAGONAL),
+                                           Edge(grid[i][j+1], COST_ALONG_BLOCK)])
+                    else:
+                        node.edges.extend([Edge(grid[i-1][j+1], COST_DIAGONAL),
+                                           Edge(grid[i][j + 1], COST_STRAIGHT),
+                                           Edge(grid[i + 1][j + 1], COST_DIAGONAL)])
+
+            # West-bound edges
+            if j > 0:
+                # if top-row
+                if i == 0:
+                    if grid[i][j-1].block and grid[i+1][j-1].block:
+                        pass  # no west edges
+                    elif grid[i][j-1].block:
+                        node.edges.extend([Edge(grid[i][j-1], COST_ALONG_BLOCK),
+                                           Edge(grid[i + 1][j - 1], COST_DIAGONAL)])
+                    elif grid[i + 1][j-1].block:
+                        node.edges.append(Edge(grid[i][j - 1], COST_ALONG_BLOCK))
+                    else:
+                        node.edges.extend([Edge(grid[i][j - 1], COST_STRAIGHT),
+                                           Edge(grid[i + 1][j - 1], COST_DIAGONAL)])
+                # if bottom-row
+                elif i == len(grid) - 1:
+                    if not node.block:
+                        node.edges.append(Edge(grid[i - 1][j - 1], COST_DIAGONAL))
+                # regular nodes
+                else:
+                    if grid[i][j-1].block and grid[i + 1][j-1].block:
+                        pass  # no east edges
+                    elif grid[i][j-1].block:
+                        node.edges.extend([Edge(grid[i][j - 1], COST_ALONG_BLOCK),
+                                           Edge(grid[i + 1][j - 1], COST_DIAGONAL)])
+                    elif grid[i + 1][j-1].block:
+                        node.edges.extend([Edge(grid[i - 1][j - 1], COST_DIAGONAL),
+                                           Edge(grid[i][j - 1], COST_ALONG_BLOCK)])
+                    else:
+                        node.edges.extend([Edge(grid[i - 1][j - 1], COST_DIAGONAL),
+                                           Edge(grid[i][j - 1], COST_STRAIGHT),
+                                           Edge(grid[i + 1][j - 1], COST_DIAGONAL)])
+
+            # north-bound edge
+            if i > 0 and j > 0:
+                if node.block and grid[i][j-1].block:
+                    pass
+                elif node.block or grid[i][j-1].block:
+                    node.edges.append(Edge(grid[i-1][j], COST_ALONG_BLOCK))
+                else: # no blocks
+                    node.edges.append(Edge(grid[i-1][j], COST_STRAIGHT))
+
+            # south-bound edge
+            if i < len(grid) - 1 and j > 0:
+                if grid[i+1][j].block and grid[i+1][j-1].block:
+                    pass
+                elif grid[i+1][j].block or grid[i+1][j-1].block:
+                    node.edges.append(Edge(grid[i+1][j], COST_ALONG_BLOCK))
+                else: # no blocks
+                    node.edges.append(Edge(grid[i+1][j], COST_STRAIGHT))
+
+
+# graph_node class: has point, h(), crime_stat, edges
+
+# tree_node class: has parent, graph_node, edges, A* score, cost, children
+
+
+# edge class: has graph_node ref, cost.
+
+# we could calculate h() on the fly just as edge
+
+# open_list
+
+# monotonous comment
+# visited_list
+
+
+# below shows range is ok
+
+# for i_shape in shapes:
+#     if i_shape.points[0][1] < 45.49 or i_shape.points[0][1] >= 45.53:
+#         print(i_shape.points[0][1])
+#         break
+#
+#
+# print("hey")
+
+# since monotonous, nodes are visited once, and thus edges are calculated once.
+# Thus it makes sense to get the edges on the fly, vs calculateing all of them beforehand
+# get_edges
+
+if __name__ == "__main__":
+    main()
